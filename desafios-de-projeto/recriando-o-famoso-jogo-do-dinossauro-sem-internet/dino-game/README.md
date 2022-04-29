f1c828ef2ae59948c590fe7188e560d9f6dd1aaf
thaianealm <cont.thaiane@gmail.com>
2022-04-29 10:36:35 -0300

    Commit inicial: Recriando o famoso jogo do dinossauro sem internet

---
+++ desafios-de-projeto/recriando-o-famoso-jogo-do-dinossauro-sem-internet/dino-game/README.md
@@ -0,0 +1,10 @@
+# Dino Game
+## 🛠 Tecnologias usadas
+- HTML5
+- CSS3
+- JavaScript
+
+## Demonstração
+
+<img src="./assets/img/dino-game-img.png">
+<img src="./assets/img/dino-game-img-2.png">
---
+++ desafios-de-projeto/recriando-o-famoso-jogo-do-dinossauro-sem-internet/dino-game/assets/js/script.js
@@ -0,0 +1,70 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        if(!isJumping){
+            jump();
+        }
+    }
+}
+
+function jump(){
+
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+            clearInterval(upInterval)
+
+            let downInterval = setInterval(() => {
+                if(position <=0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else{
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+
+        } else{
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+}, 20);
+
+}
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    //console.log(randomTime)
+
+    cactus.classList.add('cactus');
+    cactus.style.left = cactusPosition + 'px';
+    document.body.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            document.body.removeChild(cactus);
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //Game Over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo.</h1> <h2 class="restart">Recarregue a página para jogar novamente</h2>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px'
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
